1999e4600d7b5e87e419e5a8c4eed8102306d6a6
Anjali Garg <anjaligarg@dentalkart.com>
2024-08-09 16:08:11 +0530

    pushing front

--- go-kapture/src/App.js
+++ go-kapture/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Spreadsheet Application</h1>
+      <h1 className="text-2xl font-bold mb-4"> Spreadsheet Application </h1>
       <FormattingControls />
       <Grid />
      
